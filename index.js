1c80fdea6c274ac64fe60ad86ea5cd8b92b1fcac
StNeko <117987167+StNek0@users.noreply.github.com>
2023-02-25 20:22:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path') // для формирования корректных путей
+
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    console.log('server req')
+
+    res.setHeader('Content-Type', 'text/html')
+
+    const createPath = (page) => path.resolve(__dirname, 'pages', `${page}.html`)
+    // для чего: некоторые файловые системы используют прямую косую черту, а некоторые обратную!
+    let basePath = ''
+
+    // базовая имплементация роутинга
+    switch (req.url) {
+        case '/page1':
+            res.statusCode = 301
+            res.setHeader('Location', 'page1')
+            res.end()
+        case '/page2':
+            basePath = createPath('page2')
+            res.statusCode = 200
+            break
+        case '/page3':
+            basePath = createPath('page3')
+            res.statusCode = 200
+            break
+        default:
+            basePath = createPath('error')
+            res.statusCode = 404
+            break
+    }
+
+    fs.readFile('./data.json', (error, data) => {
+        res.setHeader('Content-Type', 'application/json')
+        res.write(data)
+        res.end()
+    })
+    
+
+    /* const data = JSON.stringify([
+        { name: 'Евгений Олегович', age: 123456789 },
+        { name: 'Дух Коммунизма', age: 987654321 }
+    ]) */
+    res.write(data)
+    res.end()
+
+})
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`)
+})
